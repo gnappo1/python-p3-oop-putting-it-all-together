78b2c41cd1b2b1ca2bc5ff27db5828874a3cbd9e
Matteo Piccini <gnappo.1@live.it>
2022-12-28 13:43:52 -0800

    Done

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self._page_count = 0
+    
+    def get_page_count(self):
+        print(self._page_count)
+        return self._page_count
+    
+    def set_page_count(self, count):
+        if type(count) in (int, float):
+            self._page_count = count
+        else:
+            print("page_count must be an integer")
+            return "page_count must be an integer"
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+    page_count = property(get_page_count, set_page_count,)
+
+hp = Book("Harry Potter")
+if __name__ == '__main__':
+    import ipdb; ipdb.set_trace()
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand):
+        self.brand = brand
+        self._size = None
+    
+    def get_size(self):
+        print(self._size)
+        return self._size
+    
+    def set_size(self, size):
+        if type(size) in (int, float) and 0 <= size <= 100:
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    def cobble(self):
+        self.condition = "New" if hasattr(self, "condition") else "You must set an initial condition first!"
+        print("Your shoe is as good as new!")
+    
+    size = property(get_size, set_size)
+
+stan_smith = Shoe("Adidas")
+if __name__ == '__main__':
+    import ipdb; ipdb.set_trace()
+stan_smith.size = 11
